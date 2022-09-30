6d380defef7fc3aed5367a5c40fed4426956db45
unknown <choso1097@gmail.com>
2022-09-30 12:41:57 +0900

    Initial commit

---
+++ src/Testtt.java
@@ -0,0 +1,10 @@
+public class Testtt {
+    public static void main(String[] args) {
+        int[] arr = new int[101];
+        for (int i = 1; i < arr.length; i++) {
+            int[]arr = i;
+            System.out.println(i);
+
+        }
+    }
+}
